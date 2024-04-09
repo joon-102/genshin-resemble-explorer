f9e788b49a18cc04175e4f2bd5981c9d681567e3
joon-102 <joon_1@outlook.kr>
2024-04-10 02:29:09 +0900

    Mobile optimization, bug fixes

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,16 +1,12 @@
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
-
 import styles from "../styles/Main.module.css";
 
-function upload() {
-
-}
-
-function imgUpload() {
-
-}
-
 function Main() {
+    const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log('파일 선택됨:', event.target.files);
+    };
+
     return (
         <>
             <Helmet>
@@ -21,29 +17,25 @@ function Main() {
             </Helmet>
 
             <div className={styles.container}>
-
                 <div>
-                    <h1 className={styles.message} >나와 닮은 원식 케릭터 찿기</h1>
+                    <h1 className={styles.message}>나와 닮은 원식 케릭터 찿기</h1>
                     <h2 className={styles.title}>나와 닮은 원신 케릭터를 찿아보세요!</h2>
                 </div>
                 <img src="icon.png" alt="logo" className={styles.logo} />
 
                 <div className={styles.container_2}>
-                    <div className={styles.image_upload}>
+                    <label htmlFor="fileUpload" className={styles.image_upload}>
                         <input
                             type="file"
-                            ref={upload}
-                            multiple
-                            onChange={imgUpload}
+                            id="fileUpload"
+                            onChange={handleFileUpload}
                             accept="image/*"
                             className={styles.file_upload_input}
                         />
                         <h2 className={styles.upload_text}>사진을 올려놓거나 눌러서 업로드</h2>
-                    </div>
+                    </label>
                 </div>
-
             </div>
-
         </>
     );
 }
